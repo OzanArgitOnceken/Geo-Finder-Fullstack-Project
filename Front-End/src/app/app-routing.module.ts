33eb9f815a97f436ba4951050596bfbde9b4a2f7
Ozan Argıt Önçeken <ozanargitonceken@gmail.com>
2024-03-08 19:18:45 +0300

    Routing and Navigation in Angular done
    Also title name changed

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaceListComponent } from './place-list/place-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'places',component: PlaceListComponent},
+  {path: '',redirectTo:'places',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
