315e208724cf0face49714043d4a93448fe46642
Ozan Argıt Önçeken <ozanargitonceken@gmail.com>
2024-03-09 10:25:24 +0300

    PORT Changed
    Port changed 8080 to 8070

--- Front-End/src/app/place.service.ts
+++ Front-End/src/app/place.service.ts
@@ -7,7 +7,7 @@ import { Place } from './place';
   providedIn: 'root'
 })
 export class PlaceService {
-  private baseURL="http://localhost:8080/api/v1/places";
+  private baseURL="http://localhost:8070/api/v1/places";
   constructor(private httpClient:HttpClient) { }
   getPlacesList(): Observable<Place[]> {
     return this.httpClient.get<Place[]>(`${this.baseURL}`);
